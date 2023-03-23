71891c5feec81d39facd5362816a453a65d3c426
socry <leonardosbo02@gmail.com>
2023-03-23 16:02:28 -0300

    Add function to get 8 moves from start point

---
+++ script.js
@@ -0,0 +1,32 @@
+function getPaths(start) {
+  x = [
+    start[0] + 2,
+    start[0] + 2,
+    start[0] - 2,
+    start[0] - 2,
+    start[0] + 1,
+    start[0] + 1,
+    start[0] - 1,
+    start[0] - 1,
+  ];
+  y = [
+    start[1] - 1,
+    start[1] + 1,
+    start[1] + 1,
+    start[1] - 1,
+    start[1] + 2,
+    start[1] - 2,
+    start[1] + 2,
+    start[1] - 2,
+  ];
+
+  let arr = [];
+
+  for (let i = 0; i < 8; i++) {
+    if (x[i] >= 0 && x[i] <= 7 && y[i] >= 0 && y[i] <= 7) {
+      let newArray = [x[i], y[i]];
+      arr.push(newArray);
+    }
+  }
+  return arr;
+}
