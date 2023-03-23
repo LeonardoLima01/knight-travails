ad65903cdab34270479fc988c8c83ffd550c0395
socry <leonardosbo02@gmail.com>
2023-03-23 16:02:56 -0300

    Add function to create chess array board

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ function getPaths(start) {
   }
   return arr;
 }
+function createBoard() {
+  let board = [];
+
+  for (let i = 0; i < 8; i++) {
+    board[i] = [];
+
+    for (let j = 0; j < 8; j++) {
+      board[i][j] = "";
+    }
+  }
+  return board;
+}
